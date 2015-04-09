f9e21f09f7cd974f59cb9c9db84dbccca9b4daf8
Draveness <stark.draven@gmail.com>
2015-04-09 17:06:19 +0800

    Upload accMax

--- Prolog/arithmetic.pl
+++ Prolog/arithmetic.pl
@@ -4,7 +4,6 @@ len([_|T],N):-len(T,X), N is X+1.
 accLen([_|T],A,L):-Anew is A+1,accLen(T,Anew,L).
 accLen([],A,A).
 
-length(List,Length):-accLen(List,0,Length).
 
 accMax([H|T],A,Max):-
     H > A,
