c147180c617df66c27da5eb298924b3c9411477c
Draveness <stark.draven@gmail.com>
2015-04-09 14:57:37 +0800

    Upload combine

--- Prolog/combine.pl
+++ Prolog/combine.pl
@@ -1,3 +1,12 @@
 combine1([],[],[]).
 combine1([X|Xs],[Y|Ys],[X,Y|Zs]):-
     combine1(Xs,Ys,Zs).
+
+combine2([],[],[]).
+combine2([X|Xs],[Y|Ys],[[X,Y]|Zs]):-
+    combine2(Xs,Ys,Zs).
+
+
+combine3([],[],[]).
+combine3([X|Xs],[Y|Ys],[join(X,Y)|Zs]):-
+    combine3(Xs,Ys,Zs).
