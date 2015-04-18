b32152c2c5a5788b453013e7d2ecb78ba0ccbd8d
Draveness <stark.draven@gmail.com>
2015-04-19 00:39:55 +0800

    Add register

--- nodejs/blog/app.js
+++ nodejs/blog/app.js
@@ -9,12 +9,14 @@ var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 var settings = require('./settings');
+var flash = require('connect-flash');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash())
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- nodejs/blog/routes/index.js
+++ nodejs/blog/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto'),
+	User = require('../models/user.js')
 
 module.exports = function(app) {
 	app.get('/', function(req, res) {
@@ -9,6 +11,43 @@ module.exports = function(app) {
 		res.render('reg', { title: 'Register' });
 	});
 	app.post('/reg', function(req, res) {
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password-repeat'];
+
+		if (password_re != password) {
+			req.flash('error', 'Different between inputs');
+			return res.redirect('/reg');
+		};
+
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		var newUser = new User({
+			name: name,
+			password: password,
+			email: req.body.email
+		});
+
+		User.get(newUser.name, function(err, user) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/reg');
+			}
+			if (user) {
+				req.flash('error', 'User already exists!');
+				return res.redirect('/reg');
+			};
+
+			newUser.save(function(err, user) {
+				if (err) {
+					req.flash('error', err);
+					return res.redirect('/reg');
+				};
+				res.session.user = user;
+				req.flash('success', 'Register successful!');
+				res.redirect('/');
+			});
+		});
 	});
 	app.get('/login', function(req, res) {
 		res.render('login', { title: 'Login' });
