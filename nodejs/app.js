f68188aab962be1f8adbdacda0574becf40f70fe
Draveness <stark.draven@gmail.com>
2015-04-18 16:28:35 +0800

    Init blog

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,8 +1,13 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('<h1>Node.js</h1>');
-	res.end('<p>Hello World</p>');
-}).listen(3000);
-console.log("HTTP server is listening at port 3000.");
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+
+var server = app.listen(3000, function() {
+	var host = server.address().address();
+	var port = server.address().port();
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
