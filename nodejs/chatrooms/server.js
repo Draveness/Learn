0b446acd815303aae163dda05ac937a0da97d68f
Draveness <stark.draven@gmail.com>
2015-04-25 08:43:43 +0800

    Add send file

--- nodejs/chatrooms/server.js
+++ nodejs/chatrooms/server.js
@@ -9,3 +9,33 @@ function send404(response) {
 	response.write('Error 404: resource not found.');
 	response.end();
 }
+
+
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(
+			200,
+			{"Content-type": mime.lookup(path.basename(filepath))}
+			);
+	response.end(fileContents);
+}
+
+function serverStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function(exists) {
+			if (exists) {
+				fs.readFile(absPath, function(err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
+}
