67721333a8bbb6327b38a96b336fd6db5e23f6b6
Draveness <stark.draven@gmail.com>
2015-04-25 13:34:57 +0800

    Add chat ui

---
+++ nodejs/chatrooms/public/javascripts/char_ui.js
@@ -0,0 +1,7 @@
+function divEscapedContentElement(message) {
+    return $('<div></div>').html('<i>' + message + '</i>');
+}
+
+function divSystemContentElement(message) {
+    return $('<div></div').html('<i>' + message + '</i>');
+}
