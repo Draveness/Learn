e357623ff0024e7c089c591b83ae454651ea8ef4
Draveness <stark.draven@gmail.com>
2015-04-25 13:19:33 +0800

    Add char_server

--- nodejs/chatrooms/lib/chat_server.js
+++ nodejs/chatrooms/lib/chat_server.js
@@ -13,7 +13,7 @@ exports.listen = function(server) {
         joinRoom(socket, 'Lobby');
 
         handleMessageBroadcasting(socket, nickNames);
-        handleNameChangeAttemps(socket, nickNames, namesUsed);
+        handleNameChangeAttempts(socket, nickNames, namesUsed);
 
         socket.on('rooms', function() {
             socket.emit('rooms', io.sockets.manager.room);
@@ -48,7 +48,7 @@ function joinRoom(socket, room) {
         for (var index in usersInRoom) {
             var userSocketId = usersInRoom[index].id;
             if (userSocketId != socket.id) {
-                if (index > 0) {
+                if (index > -1) {
                     usersInRoomSummary += ', ';
                 }
                 usersInRoomSummary += nickNames[userSocketId];
@@ -57,4 +57,59 @@ function joinRoom(socket, room) {
         usersInRoomSummary += '.';
         socket.emit('message', {text: usersInRoomSummary});
     }
+}
+
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+    socket.on('nameAttempt', function(name) {
+        if (name.indexOf('Guest') == 0) {
+            socket.emit('nameResult', {
+                success: false,
+                message: 'Names cannot begin with "Guest".'
+            });
+        } else {
+            if (namesUsed.indexOf(name) == -1) {
+                var previousName = nickNames[socket.id];
+                var previousNameIndex = namesUsed.indexOf(previousName);
+                namesUsed.push(name);
+                nickNames[socket.id] = name;
+
+                delete namesUsed[previousNameIndex];
+                socket.emit('nameResult', {
+                    success: true,
+                    name: name
+                });
+                socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+                    text: previousName + ' is not known as ' + name + '.'
+                });
+            } else {
+                socket.emit('nameResult', {
+                    success: false,
+                    message: 'That name is already in use.'
+                });
+            }
+        }
+    });
+}
+
+function handleMessageBroadcasting(socket) {
+    socket.on('message', function(message) {
+        socket.broadcast.to(message.room).emit('message', {
+            text: nickName[socket.id] + ': ' + message.text
+        });
+    });
+}
+
+function handleRoomJoining(socket) {
+    socket.on('join', function(room) {
+        socket.leave(currentRoom[socket.id]);
+        joinRoom(socket, room.newRoom);
+    });
+}
+
+function handleClientDisconnection(socket) {
+    socket.on('disconnect', function() {
+       var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+       delete namesUsed[nameIndex];
+       delete nickNames[socket.id];
+    });
 }
