ecf3524af87e5931ccd2bfc49a6dde476107b75d
Draveness <stark.draven@gmail.com>
2015-04-28 16:44:22 +0800

    Add ABS

---
+++ Advanced-Bash-Scripting/shift-past.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# shift-past.sh
+
+shift 3
+#  n=3; shift $n
+#  Has the same effect.
+
+echo "$1"
+
+exit 0
---
+++ Advanced-Bash-Scripting/uppercase.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# uppercase.sh : Change input to uppercase.
+
+
+tr 'a-z' 'A-Z'
+#  Letter ranges must be quoted
+#+ to prevent filename generation from single-letter filename.
+
+exit 0
